85cb1709d74f05bd0c59159c1b51616f3d8d200b
christianrafael21 <christianrafael102@gmail.com>
2025-10-28 00:09:22 -0300

    Fix: Corrige basePath para PortfolioChristianDev

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/PortfolioChristianDev' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/PortfolioChristianDev/' : '',
 }
 
 module.exports = nextConfig
